c0a0e63440aa0490e1287d1dae591569ac8ffc3a
DanielRY <danielrojasy@upeu.edu.pe>
2020-05-12 12:29:27 -0500

    daniel

--- js/pregunta3.js
+++ js/pregunta3.js
@@ -2,13 +2,16 @@ var nombre=prompt("Nombre: "),edad=parseFloat(prompt("Edad: ")),salario=parseFlo
 if (edad<16) {
     document.write("Lo sentimos usted no tiene edad para trabajar");
 } else {
-    document.write("Bienvenido: ",nombre);
+    alert("Bienvenido: ",nombre)
     if(edad>=19&&edad<=50) {
-        nuevosalario=salario+(salario*0.05);
+        alert("Felicitaciones usted tiene una bonifacion de 5%");
+        salario+=(salario*0.05);
     }else if(edad>=51&&edad<=60){
-        nuevosalario=salario+(salario*0.1);
-    }else{
-        nuevosalario=salario+(salario*0.15);
+        alert("Felicitaciones usted tiene una bonifacion de 10%");
+        salario+=(salario*0.1);
+    }else if(edad>60){
+        alert("Felicitaciones usted tiene una bonifacion de 45%");
+        salario+=(salario*0.15);
     }
+    document.write("Salario: ",salario);
 }
-document.write("Salario: ",nuevosalario);
